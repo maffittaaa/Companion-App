d9a653199b4e1123379b83d2e75285f4ab8f8633
Maffittaa <mafaldamartins2001@gmail.com>
2024-10-17 10:43:05 +0100

    Starting Companion App TR

---
+++ app/src/main/java/pt/iade/games/companionapp/userInterface/CompanionApp.kt
@@ -0,0 +1,24 @@
+package pt.iade.games.companionapp.userInterface
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun UICompanionApp()
+{
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "The Rumble",
+            fontSize = 20.sp
+        )
+
+    }
+}
