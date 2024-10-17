bb674aaeea71556659714809a6d76897195aedeb
artugatuga <148588412+artugatuga@users.noreply.github.com>
2024-10-17 11:15:45 +0100

    Update CompanionApp.kt

--- app/src/main/java/pt/iade/games/companionapp/ui/userInterface/CompanionApp.kt
+++ app/src/main/java/pt/iade/games/companionapp/ui/userInterface/CompanionApp.kt
@@ -1,7 +1,6 @@
-package pt.iade.games.companionapp.userInterface
+package pt.iade.games.companionapp.ui.userInterface
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
